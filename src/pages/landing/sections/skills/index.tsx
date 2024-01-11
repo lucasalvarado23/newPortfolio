ac685628c93e5f7bf2e0b1dbd325f392a6271576
Lucas Alvarado <lucasalvarado23@gmail.com>
2024-01-11 11:03:34 -0500

    fixed typescript build errors

--- src/pages/landing/sections/skills/index.tsx
+++ src/pages/landing/sections/skills/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Styles
 import './style.css';
@@ -35,7 +35,6 @@ function Skills() {
   return (
     <section id="skills" className="section">
       <h2 className="title">{skillsData.title}</h2>
-      <div className="section-des">{skillsData.description}</div>
       <p
         className="content-670"
         dangerouslySetInnerHTML={{
@@ -43,28 +42,13 @@ function Skills() {
         }}></p>
 
       <ul className="skills-history block-right">
-        {skillsData.skillsHistory.map((skill, i: number) => (
-          <li key={'skill=item-' + i}>
+        {skillsData.skillsHistory.map((skill, i) => (
+          <li key={'skill-item-' + i}>
             <span className="date">{skill.title}</span>
             <p
               dangerouslySetInnerHTML={{
                 __html: markdownToHTML(skill.description),
               }}></p>
-          </li>
-        ))}
-      </ul>
-
-      <ul className="skills-progress">
-        {skillsData.skillsProgress.map((prog, i) => (
-          <li key={'prog' + i}>
-            <span className="name">{prog.name}</span>
-            <div className="skill">
-              <div
-                className="skill-fill"
-                style={{
-                  width: progressReached ? `${prog.percentage}%` : 0,
-                }}></div>
-            </div>
           </li>
         ))}
       </ul>
